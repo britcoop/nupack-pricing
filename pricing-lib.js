a23d60117013598c90ddd130bdf48a367506dac3
Brittany Cooper <brittany@Brittanys-MacBook-Pro.local>
2017-03-04 14:19:14 -0500

    method improvements
    fixed several calculation errors, added toFixed method

--- pricing-lib.js
+++ pricing-lib.js
@@ -13,29 +13,37 @@ function calcMarkUp(priceData){
 	}
 
 	//fixed markup of 5%
-	priceData.basePrice = priceData.basePrice + priceData.basePrice * 0.05;
+	var fixedMarkUp = priceData.basePrice + priceData.basePrice * 0.05;
+	//total price count
+	var totalPrice = fixedMarkUp;
 
 	//markup for pharmaceuticals 7.5%
 	if (priceData.materialType === "pharma") {
 		markUpPrice = priceData.basePrice + priceData.basePrice * 0.075;
+		totalPrice = markUpPrice;
 	}
 	//markup for food 13%
 	else if (priceData.materialType === "food") {
 		markUpPrice = priceData.basePrice + priceData.basePrice * 0.13;
+		totalPrice = markUpPrice;
 	}
 	//markup for electronics 2%
 	else if (priceData.electronics === "electronics") {
-		markUpPrice = bpriceData.basePrice + priceData.basePrice * 0.02;
+		markUpPrice = priceData.basePrice + priceData.basePrice * 0.02;
+		totalPrice = markUpPrice;
 	}
 	else {
 		markUpPrice = basePrice;
 	}
 
+	totalPrice = markUpPrice.toFixed(2);
+	priceData.basePrice = priceData.basePrice.toFixed(2);
+
 	//output
 	output += "Example " + priceData.exampleNum + ": <br>";
 	output += "-------------- <br>";
 	output += "Input: " + priceData.basePrice + ", " + priceData.numberOfPeople + " " + personPlural + ', ' + priceData.materialType + "<br>";
-	output += "Output: " + markUpPrice;
+	output += "Output: " + totalPrice;
 
 	return output;
 }
